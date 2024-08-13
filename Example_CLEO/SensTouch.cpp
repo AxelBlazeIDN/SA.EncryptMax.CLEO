11740deacea77fb83fa20d6bea12c13b592c9132
Axel Blaze <axelblazeindo@gmail.com>
2024-08-13 13:55:32 +0700

    Update SensTouch.cpp

--- Example_CLEO/SensTouch.cpp
+++ Example_CLEO/SensTouch.cpp
@@ -8,7 +8,7 @@ jump @AB_SensTouch
 0662: printstring "if possible the cleo code is broken and not messed up" 
 0662: printstring "You won't be able to steal or re-edit this cleo Lol" 
 0662: printstring "Encrypted By Axel Blaze" 
-{$Include Include\Encrypt_1.h}
+{$Include Includes\Encrypted_1.h}
 
 // This example script was created by Axel Blaze
 // SanAndreasMixMod.blogspot.com
@@ -20,4 +20,4 @@ const SensVAddres = 0x6A9F30
 0DD9: write_mem_addr SensVAddres value SenSvalue size 4 add_ib 1 protect 1
 return
 
-{$Include Include\Encrypt_2.h}
+{$Include Includes\Encrypted_2.h}
