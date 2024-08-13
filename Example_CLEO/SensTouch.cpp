c833aea7887ed76240b4c35c97cd6b912f9aed72
Axel Blaze <axelblazeindo@gmail.com>
2024-08-13 13:48:58 +0700

    Update SensTouch.cpp

--- Example_CLEO/SensTouch.cpp
+++ Example_CLEO/SensTouch.cpp
@@ -1,5 +1,12 @@
 {$CLEO .csa}
 
+:EncryptMax
+0000: NOP
+jump @AB_SensTouch
+0662: printstring "You won't be able to compile cleo on this one locked" 
+0662: printstring "if possible the cleo code is broken and not messed up" 
+0662: printstring "You won't be able to steal or re-edit this cleo Lol" 
+0662: printstring "Encrypted By Axel Blaze" 
 {$Include Include\Encrypt_1.h}
 
 // This example script was created by Axel Blaze
