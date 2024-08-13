89a64af81b450462458af6818a6851ddb105eab4
Axel Blaze <axelblazeindo@gmail.com>
2024-08-13 13:58:34 +0700

    Update SensTouch.cpp

--- Example_CLEO/SensTouch.cpp
+++ Example_CLEO/SensTouch.cpp
@@ -2,12 +2,12 @@
 jump @EncryptMax
 
 :EncryptMax
-0000: NOP
-jump @AB_SensTouch
-0662: printstring "You won't be able to compile cleo on this one locked" 
-0662: printstring "if possible the cleo code is broken and not messed up" 
-0662: printstring "You won't be able to steal or re-edit this cleo Lol" 
-0662: printstring "Encrypted By Axel Blaze" 
+    0000: NOP
+    jump @AB_SensTouch
+    0662: printstring "You won't be able to compile cleo on this one locked" 
+    0662: printstring "if possible the cleo code is broken and not messed up" 
+    0662: printstring "You won't be able to steal or re-edit this cleo Lol" 
+    0662: printstring "Encrypted By Axel Blaze" 
 {$Include Includes\Encrypted_1.h}
 
 // This example script was created by Axel Blaze
@@ -16,8 +16,8 @@ float SensValue
 const SensVAddres = 0x6A9F30
 
 :AB_SensTouch
-0AF2: SensValue = read_float_from_ini_file "AxelBlaze\SensTouchSA.ini" section "SensTouch" key "SettValue" // com.rockstargames.gtasa/files/AxelBlaze/SensTouchSA.ini
-0DD9: write_mem_addr SensVAddres value SenSvalue size 4 add_ib 1 protect 1
+    0AF2: SensValue = read_float_from_ini_file "AxelBlaze\SensTouchSA.ini" section "SensTouch" key "SettValue" // com.rockstargames.gtasa/files/AxelBlaze/SensTouchSA.ini
+    0DD9: write_mem_addr SensVAddres value SenSvalue size 4 add_ib 1 protect 1
 return
 
 {$Include Includes\Encrypted_2.h}
