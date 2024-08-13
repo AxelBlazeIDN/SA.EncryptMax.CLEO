0ce58809e668194867c51a2830828c6e74202719
Axel Blaze <axelblazeindo@gmail.com>
2024-08-13 13:42:23 +0700

    Update and rename Sens_TouchSA.cpp to SensTouch (Improved).cpp

--- Example_CLEO/SensTouch (Improved).cpp
+++ Example_CLEO/SensTouch (Improved).cpp
@@ -357,8 +357,8 @@ float SensValue
 const SensVAddres = 0x6A9F30
 
 :AB_SensTouch
-0AF2: SensValue = read_float_from_ini_file "AxelBlaze\SensTouchSA.ini" section "SensTouch" key "SettValue" // com.rockstargames.gtasa/files/AxelBlaze/SensTouchSA.ini
-0DD9: write_mem_addr SensVAddres value SenSvalue size 4 add_ib 1 protect 1
+    0AF2: SensValue = read_float_from_ini_file "AxelBlaze\SensTouchSA.ini" section "SensTouch" key "SettValue" // com.rockstargames.gtasa/files/AxelBlaze/SensTouchSA.ini
+    0DD9: write_mem_addr SensVAddres value SenSvalue size 4 add_ib 1 protect 1
 return
 
 hex
